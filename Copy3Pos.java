8f025a8d492e4884968a5998a0f04a89421d44c6
MayurJangale2013 <124575744+MayurJangale2013@users.noreply.github.com>
2023-02-07 10:15:02 +0530

    Update Copy3Pos.java

--- Copy3Pos.java
+++ Copy3Pos.java
@@ -5,7 +5,7 @@ class Copy3Pos
 	int [] a = new int [] {10,20,30,40,50,60,70,80,90,100};
 	int [] b = new int [3];
 	int j = 0;
-	for(int k=3;k<=5;k++)
+	for(int k=4;k<=5;k++)
 	{
 		b[j] = a[k];
 		j++;
