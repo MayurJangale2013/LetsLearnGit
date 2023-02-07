33ee80ab5937039b7bd99a3913f3c5a5027c0a8d
Mayur.jangale <mayur.jangale2013@gmail.com>
2023-02-07 09:45:44 +0530

    New code added

---
+++ Copy3Pos.java
@@ -0,0 +1,19 @@
+class Copy3Pos
+{
+	public static void main(String [] args)
+{
+	int [] a = new int [] {10,20,30,40,50,60,70,80,90,100};
+	int [] b = new int [3];
+	int j = 0;
+	for(int k=3;k<=5;k++)
+	{
+		b[j] = a[k];
+		j++;
+	}
+	System.out.println("Copied array from 3rd to 5th position from array ");
+	// j=0;
+	// while(j<b.length)
+	for(j=0;j<b.length;j++)
+		System.out.print(b[j] + " ");
+}
+}
